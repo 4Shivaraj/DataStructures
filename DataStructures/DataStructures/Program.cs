602801962bc63c44571212edc906a0de53c8f909
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-21 03:51:06 +0530

    [Shivaraj] Added Add Node Problem in Linked list

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//Console.WriteLine("Welcome to Data Structures using Generics");
 namespace DataStructure
 {
     class Program
@@ -16,20 +15,38 @@ namespace DataStructure
             LinkedLists linkedLists = new LinkedLists();
 
             //UC-1
-            Node node1 = new Node(56);
-            Node node2 = new Node(30);
-            Node node3 = new Node(70);
-            node1.next = node2;
-            node2.next = node3;
-            linkedLists.Head = node1;
+            //Node node1 = new Node(56);
+            //Node node2 = new Node(30);
+            //Node node3 = new Node(70);
+            //node1.next = node2;
+            //node2.next = node3;
+            //linkedLists.Head = node1;
+            //linkedLists.Display();
+            Console.WriteLine("\n-------------------------------");
+
+            //UC-2
+            linkedLists.AddNode(70);
+            linkedLists.AddNode(30);
+            linkedLists.AddNode(56);
             linkedLists.Display();
+            Console.WriteLine("\n-------------------------------");
 
         }
     }
 }
 
-//UC-1
-//Lets create a simple Linked List of 56, 30 and 70
+// UC - 2
+//Ability to create Linked
+//List by adding 30 and 56 to 70
+
+//- Node with data 70 is First Created
+//- Next 30 is added to 70
+//- Finally 56 is added to 30
+//- LinkedList Sequence: 56->30->70
 
 //Result
-//56 -> 30 -> 70
+//Added 70 at start of the linkedlist
+//Added 30 at start of the linkedlist
+//Added 56 at start of the linkedlist
+//Linked List:
+//56-> 30-> 70
