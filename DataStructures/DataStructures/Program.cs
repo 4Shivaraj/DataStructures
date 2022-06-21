496f69e97cfeaeebaafdecbe9a08f0b1f951698c
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-21 05:42:06 +0530

    [Shivaraj] Implemented Inserting New Node Value, Problem in Linked list

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -63,36 +63,46 @@ namespace DataStructure
             //linkedLists.Display();
             //Console.WriteLine("\n-------------------------------");
 
-            //UC-7
-            Node node1 = new Node(56);
-            Node node2 = new Node(30);
-            Node node3 = new Node(70);
-            node1.next = node2;
-            node2.next = node3;
-            linkedLists.Head=node1;
-            Console.WriteLine("Search For Node value 30");
+            ////UC-7
+            //Node node1 = new Node(56);
+            //Node node2 = new Node(30);
+            //Node node3 = new Node(70);
+            //node1.next = node2;
+            //node2.next = node3;
+            //linkedLists.Head=node1;
+            //Console.WriteLine("Search For Node value 30");
+            //linkedLists.Display();
+            //if (linkedLists.Search(30))
+            //{
+            //    Console.WriteLine("\nNode with value 30 is available in Linked list");
+            //}
+            //Console.WriteLine("\n-------------------------------");
+
+
+            //UC-8
+            Console.WriteLine("Add Node After Node value 30 ");
+            linkedLists.AppendNode(56);
+            linkedLists.AppendNode(30);
+            linkedLists.AppendNode(70);
+            linkedLists.InsertNode(40,30);
             linkedLists.Display();
-            if (linkedLists.Search(30))
-            {
-                Console.WriteLine("\nNode with value 30 is available in Linked list");
-            }
-            Console.WriteLine("\n-------------------------------");
-
-
-
-
 
         }
     }
 }
 
-//UC-7
-//Ability to search LinkedList to find Node with value 30
+//UC-8
+//Ability to insert 40 after 30 to the Linked List sequence of 56->30->70
 //- Write MSTest Test Case as demonstrated in class
-//-Loop through LinkedList to find node with key 30
+//-Search LinkedList to get Node with key value 30
+//- Then Insert 40 to 30
+//- Final Sequence: 56->30->40->70
 
 //Result
-//Search For Node value 30
+//Add Node After Node value 30
+//Added 56 at start of the linkedlist
+//Added 30 at start of the linkedlist
+//Added 70 at start of the linkedlist
+//Added at in between: 40
 // Linked List :
-//56-> 30-> 70
-//Node with value 30 is available in Linked list
+//56-> 30-> 40-> 70
